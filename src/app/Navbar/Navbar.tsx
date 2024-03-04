28473cad2e30cc82702b5f914bf9e08e845244dc
walternagawo <111139548+Nagawhos@users.noreply.github.com>
2024-03-04 11:45:23 -0400

    :lipstick: feat(Navbar): Estilização

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -27,8 +27,7 @@ export default async function Navbar() {
       <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
         <div className="flex-1">
           <Link href="/" className="btn-ghost btn text-xl normal-case">
-            <Image src={logo} height={40} width={40} alt="KodigosStore logo" />
-            KodigosStore
+            <Image src={logo} height={120} width={120} alt="KodigosStore logo" />
           </Link>
         </div>
         <div className="flex-none gap-2">
