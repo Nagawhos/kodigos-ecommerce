0a32e9df4fbfb642387a296bb12d2eb68964d685
walternagawo <111139548+Nagawhos@users.noreply.github.com>
2024-03-07 03:51:57 -0400

    :recycle: refactor: Passando para arrow functions

---
+++ frontend/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      { hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
